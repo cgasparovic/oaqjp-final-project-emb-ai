ce7a043d84912d5fb2b2dcc6e2c9ea2a68cc117b
cgasparovic <cgasparovic@gmail.com>
2025-06-12 16:03:27 -0400

    Task 7 - Update Error Handling Logic
    Figuring out error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,13 +1,14 @@
-'''
+"""
 Emotion Detection Module
-'''
+"""
 import requests
+from requests import Response
 from flask import json
 
 def emotion_detector(text_to_analyze):
-    '''
+    """
     Call the emotion detection service and return response
-    '''
+    """
     # Define the URL for the emotion detection API
     url = 'https://sn-watson-emotion.labs.skills.network/v1/' \
     'watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -15,22 +16,28 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze } }
     # Set the headers with the required model ID for the API
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    is_sample_response = False
+    flexible_response = Response()
 
     # Make a POST request to the API with the payload and headers
     # Added a timeout for local testing and error handling
     try:
-        # try request and set timeout
+        # Try request and set timeout
         response = requests.post(url, json=myobj, headers=header, timeout=2)
+        flexible_response.status_code = response.status_code
         # Parse the response from the API
         formatted_response = json.loads(response.text)
+
 
     except requests.exceptions.Timeout:
         # Handle Timeout Exception which may happen in local testing due
         # to permissions to IBM APIs
-
         print("\nRequest timed out...loading sample response...\n")
 
-        # Use Sample Response due to local access to IBM NLM APIs
+        # Fake a 200 Response
+        flexible_response.status_code = 200
+
+        # Use Sample Response due to lack local access to IBM NLM APIs
         sample_response_path = 'static/sample_responses/sample_response.txt'
         file_path = sample_response_path
 
@@ -38,14 +45,15 @@ def emotion_detector(text_to_analyze):
         with open(file_path, 'r', encoding="utf-8") as file:
             formatted_response = json.loads(file.read())
 
+        # Set that we are using a sample request
+        is_sample_response = True
+
     except requests.exceptions.RequestException as e:
-       # Handle General Request Exeptions and print exception details
+        # Handle General Request Exeptions and print exception details
         print(f"An error occurred: {e}")
 
-    # Build Result JSON
-    # Get Emotion scores
+    # Load Emotion Scores
     emotion_scores = formatted_response['emotionPredictions'][0]['emotion']
-
     # Determine the dominant emotion from response
     dominant_emotion = get_dominant_emotion_key(formatted_response)
 
@@ -59,6 +67,10 @@ def emotion_detector(text_to_analyze):
         'dominant_emotion': dominant_emotion
     }
 
+    if flexible_response == 400:
+        emotion_scores = {key: None for key in ['emotion']}
+        dominant_emotion = None
+    
     return formatted_output
 
 def get_dominant_emotion_key(data):
